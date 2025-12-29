f17785b17b4395c5513f7ae79cdca8a20af10c56
Arnol Garzón <arnol_garzon@hotmail.com>
2025-12-28 22:13:03 -0500

    chore: initial setup of Cumplo project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Cumplo</h1>
+      <p>Pequeños hábitos, grandes promesas cumplidas.</p>
+    </div>
+  );
+}
+
+export default App;
+
